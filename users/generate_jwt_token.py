3f0709d6f8f7b6d02c4a4c49a8a3baaf3d2d1bd1
Mahdi-jahanfar-dev <mahdi.jahanfar01@gmail.com>
2025-08-18 18:34:29 +0330

    feat: added get_authenticated user

--- users/generate_jwt_token.py
+++ users/generate_jwt_token.py
@@ -1,6 +1,12 @@
 import jwt
+from jwt.exceptions import InvalidTokenError, ExpiredSignatureError, DecodeError
 from config import settings
 from datetime import datetime, timezone, timedelta
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.exceptions import HTTPException
+from fastapi import Depends, status
+
+security = HTTPBearer()
 
 
 def jwt_access_token_generator(user_id: int, count: int = 30):
@@ -31,3 +37,19 @@ def jwt_refresh_token_generator(user_id: int, count: int = 1):
     
     refresh_token = jwt.encode(payload, settings.JWT_SECRET_KEY, algorithm="HS256")
     return refresh_token
+
+def get_authenticated_user(credential: HTTPAuthorizationCredentials = Depends(security)):
+    
+    token = credential.credentials
+    
+    try:
+        decoded_token = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
+        return decoded_token
+    except InvalidTokenError:
+        raise HTTPException(detail="invalid token", status_code=status.HTTP_401_UNAUTHORIZED)
+    except ExpiredSignatureError:
+        raise HTTPException(detail="token is expired", status_code=status.HTTP_401_UNAUTHORIZED)
+    except DecodeError:
+        raise HTTPException(detail="authentication faild", status_code=status.HTTP_401_UNAUTHORIZED)
+    except Exception as e:
+        raise HTTPException(detail=f"error: {e}", status_code=status.HTTP_401_UNAUTHORIZED)
